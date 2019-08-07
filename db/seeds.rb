9a79a18bb1c1d6e57a4b5948617623d65c34bc3c
Jaime Cruz <jaimecruza@me.com>
2019-08-07 16:02:36 +0200

    rails yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'french'
+
+  },
+  {
+    name:         'Bocca di Bonifaccio',
+    address:      'Bailen 19, Barcelona',
+    category:     'italian'
+  },
+  {
+    name:         'Takumi Tonoktsu',
+    address:      'Bailen 1, Barcelona',
+    category:     'japanese'
+  },
+  {
+    name:         'Tacos Orinoco',
+    address:      'Orinoco 113, Monterrey',
+    category:     'belgian'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
